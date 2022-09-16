62be52010146248846e90b8f104ef7bd8b648b8e
HyagoRodrigues <hyago-rodrigues@live.com>
2022-09-16 17:45:59 -0300

    refac

--- src/Main.groovy
+++ src/Main.groovy
@@ -1,5 +1,6 @@
 static void main(String[] args) {
         println "Digite a Entrada: "
+        //String entrada = "..P...O.....";
         String entrada = System.in.newReader().readLine();
         Portao.controle(entrada);
 }
